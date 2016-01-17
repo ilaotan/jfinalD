289d5c0a6efb2f416a3efd1f4ca51c67cf5c5331
ilaotan <tanliansheng2011@gmail.com>
2016-01-17 23:30:41 +0800

    升级到jfinal2.1

--- src/main/java/com/jfinalD/application/front/IndexController.java
+++ src/main/java/com/jfinalD/application/front/IndexController.java
@@ -1,20 +1,10 @@
 package com.jfinalD.application.front;
 
-import java.sql.SQLException;
-import java.util.List;
-
 import com.jfinal.core.Controller;
-import com.jfinal.ext.route.ControllerBind;
-import com.jfinal.plugin.ehcache.CacheKit;
-import com.jfinal.plugin.ehcache.IDataLoader;
-import com.jfinal.plugin.redis.Cache;
-import com.jfinal.plugin.redis.Redis;
-import com.jfinalD.application.system.model.Menu;
 
 /** 
  * Create by tanliansheng on 2015年10月29日
  */
-@ControllerBind(controllerKey="/",viewPath="/ftl/front")
 public class IndexController extends Controller {
 	
 	//@Before({Tx.class,CacheInterceptor.class})//EvictInterceptor.class 清除缓存
